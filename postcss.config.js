ad2ba2caffc5e6dc0ce2d4e9b1b09d714a50a126
SergeyKov <skov82@ya.ru>
2022-09-15 14:19:18 +0300

    add environment settings

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        require('postcss-nested'),
+        require('cssnano')({preset: 'default'}),
+        require('postcss-import')
+    ]
+}
